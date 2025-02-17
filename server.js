1d4ec10c0b757b985fee03497ab937eafc4a85d6
Fishwarrior06 <hectordavidortegagarcia@gmail.com>
2025-02-17 17:15:02 -0600

    We are going to a VPS

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Servir archivos estáticos
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
